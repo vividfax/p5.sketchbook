eabca5090b38a4d0276d1ff0ae28a01899bc0bb1
vividfax <vividfax@gmail.com>
2022-12-07 02:06:49 +0000

    Format for guide

--- examples/2Dand3D/sketch.js
+++ examples/2Dand3D/sketch.js
@@ -5,31 +5,26 @@ function setup() {
     createCanvas(500, 500);
     book = new Sketchbook();
 
-    book.newPage(); // red
-    book.paper.background(100, 0, 0);
-
-    book.newPage(); // green
-    book.paper.background(0, 100, 0);
+    book.newPage();
     book.value.radius = 200;
+    book.paper.background(0, 100, 0);
 
-    book.newPage("3D"); // blue
-    book.paper.background(0, 0, 225);
+    book.newPage("3D");
     book.value.radius = 0;
 
     book.run();
-    // book.viewPage(1);
 }
 
 function draw() {
 
-    book.onPage(1); // green
+    book.onPage(0);
     book.value.radius--;
     book.paper.ellipse(100, 100, 100);
     book.paper.rect(book.value.radius, 100, 10);
 
-    book.onPage(2); // blue
-    book.paper.background(0, 0, 225);
+    book.onPage(1);
     book.value.radius++;
+    book.paper.background(0, 0, 225);
     book.paper.sphere(book.value.radius);
 
     book.draw();
@@ -38,5 +33,4 @@ function draw() {
 function mousePressed() {
 
     book.next();
-    // book.reset();
 }
